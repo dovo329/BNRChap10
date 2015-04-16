08846081ba7fd6f591301b920871a6caab2431c3
Doug Voss <douglas.c.voss@gmail.com>
2015-04-16 08:36:13 -0600

    completed silver challenge

--- BNRChap10/BNRDetailViewController.m
+++ BNRChap10/BNRDetailViewController.m
@@ -21,6 +21,31 @@
 
 @implementation BNRDetailViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
+    numberToolbar.barStyle = UIBarStyleBlackTranslucent;
+    numberToolbar.items = [NSArray arrayWithObjects:
+                           [[UIBarButtonItem alloc]initWithTitle:@"Cancel" style:UIBarButtonItemStyleBordered target:self action:@selector(cancelNumberPad)],
+                           [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc]initWithTitle:@"Apply" style:UIBarButtonItemStyleDone target:self action:@selector(doneWithNumberPad)],
+                           nil];
+    [numberToolbar sizeToFit];
+    self.valueField.inputAccessoryView = numberToolbar;
+}
+
+-(void)cancelNumberPad{
+    [self.valueField resignFirstResponder];
+    self.valueField.text = @"";
+}
+
+-(void)doneWithNumberPad{
+    NSString *numberFromTheKeyboard = self.valueField.text;
+    [self.valueField resignFirstResponder];
+}
+
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
